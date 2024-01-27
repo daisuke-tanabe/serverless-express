37991ea2872c6e1c54a7f2ab09af68cc47fdaa97
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-01-27 21:36:14 +0900

    WIP dynamoDBをローカルで動かす

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import serverlessExpress from '@codegenie/serverless-express';
 import express from "express";
 
 const app = express();
+
+const client = new DynamoDBClient({
+  endpoint: 'http://localhost:8000',
+  region: 'ap-northeast-1',
+  credentials: {
+    accessKeyId: 'MockAccessKeyId',
+    secretAccessKey: 'MockSecretAccessKey'
+  },
+})
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
